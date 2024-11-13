e1978c6ac748fb005cfe38d9d70ee339383b2592
Admin <Admin@DESKTOP-7R24PKV>
2024-11-13 10:13:51 +0530

    initial commit

---
+++ pratice/src/pratice/employeeDemo.java
@@ -0,0 +1,39 @@
+package pratice;
+
+public class employeeDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int m=100;
+		employee e1=new employee();
+		employee e2=new employee();
+		employee e3=new employee();
+		
+		e1.setEid(17);
+		e1.setEname("deva");
+		e1.setCity("villupuram");
+		
+		e2.setEid(18);
+		e2.setEname("rose");
+		e2.setCity("chennai");
+		
+		e3.setEid(7);
+		e3.setEname("naruto");
+		e3.setCity("hidden leaf");
+		
+		System.out.println(e1);
+		System.out.println(e2);
+		System.out.println(e3);
+		
+
+
+		
+
+
+
+	}
+
+
+	}
+
+
